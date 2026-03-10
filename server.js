a72a4a371fa14b6b78e3af73c273fdce3d2adf63
Imran Jaleel <imranjaleel2331@gmail.com>
2026-03-10 11:50:30 +0530

    added CORS

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const { PORT } = require("./config/env");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
-
+const cors = require("cors");
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // Connect to MongoDB
 connectDB();
